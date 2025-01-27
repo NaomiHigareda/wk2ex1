bfc8be5451f9ac8094d3c0ea2f0c939120fe8ff4
NaomiHigareda <nfhigareda@amarillocollege.com>
2025-01-26 19:50:38 -0600

    Grader

--- wk2ex1/Program.cs
+++ wk2ex1/Program.cs
@@ -15,31 +15,37 @@ namespace wk2ex1
             //prompt user to insert a numeric score
             Console.WriteLine("What is a numeric score (0 to 100)?");
             int score = int.Parse(Console.ReadLine());
+
             //calculate the letter grade
             string grade = GetGrade(score);
             //prompt user to enter student grade
-            Console.WriteLine($"The students grades is: {grade}");
+            Console.WriteLine($"The letter grade for score is: {grade}");
         }
         //calculate the letter grade based on numeric grade
-        static string GetGrade(int score)
+        static string GetGrade(double score)
         {
-            if (score >= 90)
+            //if score is 89.5 between and 100 return an a
+            if (score >= 89.5 && score <=100)
             {
                 return "A";
             }
-            else if (score >= 80)
+            //if the score is between 79.5 and 89.5 return a B
+            else if (score >= 79.5 && score <= 89.5)
             {
                 return "B";
             }
-            else if (score >= 70)
+            //if the score is between 69.5 and 79.5 return C
+            else if (score >= 69.5 && score <= 79.5)
             {
                 return "C";
             }
-            else if (score >= 60)
+            //if the score is between 59.5 and 69.5 return D
+            else if (score >= 59.5 && score <= 69.5)
             {
                 return "D";
             }
-            else
+            //if the score is below 59.5 return F
+            else 
             {
                 return "F";
             }
